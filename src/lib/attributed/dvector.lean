21358ffd6746d838f76b6d1a275d361d5ea66125
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2019-11-04 22:06:52 -0500

    Cull dvector even further.

--- src/lib/attributed/dvector.lean
+++ src/lib/attributed/dvector.lean
@@ -11,19 +11,11 @@ Authors: Jesse Han, Floris van Doorn
 -/
 
 
-universes u v w
-
-@[derive decidable_eq]inductive dvector (α : Type u) : ℕ → Type u
-| nil {} : dvector 0
-| cons : ∀{n} (x : α) (xs : dvector n), dvector (n+1)
-
-@[derive decidable_eq]inductive dfin : ℕ → Type
+inductive dfin : ℕ → Type
 | fz {n} : dfin (n+1)
 | fs {n} : dfin n → dfin (n+1)
 
 namespace dfin
-
-instance has_zero_dfin {n} : has_zero $ dfin (n+1) := ⟨fz⟩
 
 instance has_one_dfin : ∀ {n}, has_one (dfin (nat.succ n))
 | 0 := ⟨fz⟩
